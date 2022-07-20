fe261682ad7b4d64787ee8a39542385cdc97ff9f
21030296 <21030296@10.174.115.49>
2022-07-20 13:31:30 +0800

    lesson10 : update resourceCentre

--- ResourceCentre_Refactor/src/Chromebook.java
+++ ResourceCentre_Refactor/src/Chromebook.java
@@ -14,6 +14,9 @@ public class Chromebook extends Item{
 	public String toString(){
 		String output = "";
 		// Write your codes here
+		output = super.toString();
+		
+		output =String.format("%-63s %-20s", output,os);
 		return output;
 	}
 }
